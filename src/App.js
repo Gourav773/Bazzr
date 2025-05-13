737bbae995f665caf7591e5f992d0b35cf53b806
gourav <gourav003yadav@gmail.com>
2025-05-14 02:27:41 +0530

    Changes in App.js file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
+// // import React from 'react';
+// // import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// // import Login from './Admin/Login.jsx';
+// // import Sidebar from './Admin/Sidebar.jsx';
+// // import Dashboard from './Admin/Dashbord.jsx';
+// // import User from './Admin/User';
+// // import Adduser from './Admin/Adduser.jsx';
+// // import Roles from './Admin/Role.jsx';
+// // import Category from './Admin/Category';
+// // import AddSubCategory from './Admin/Subcategory';
+// // import Product from './Admin/Product.jsx';
+// // import Offers from './Admin/Offers';
+// // import Charts from './Admin/Chart.jsx';
+// // import './App.css';
+
+// // function App() {
+// //   return (
+// //     <BrowserRouter basename="/Bazzr">
+// //       <Routes>
+// //         {/* Login page */}
+// //         <Route path="/login" element={<Login />} />
+
+// //         {/* Layout with Sidebar */}
+// //         <Route path="/" element={<Sidebar />}>
+// //         <Route path="/login" element={<Login />} />
+// //           <Route path="dashboard" element={<Dashboard />} />
+// //           <Route path="charts" element={<Charts />} />
+// //           <Route path="user" element={<User />} />
+// //           <Route path="adduser" element={<Adduser />} />
+// //           <Route path="role" element={<Roles />} />
+// //           <Route path="category" element={<Category />} />
+// //           <Route path="addSubCategory" element={<AddSubCategory />} />
+// //           <Route path="offers" element={<Offers />} />
+// //           <Route path="product" element={<Product />} />
+// //         </Route>
+// //       </Routes>
+// //     </BrowserRouter>
+// //   );
+// // }
+
+// // export default App;
+
+
+
+// import React from 'react';
+// import { HashRouter, Routes, Route } from 'react-router-dom';
+// import Login from './Admin/Login';
+// import Sidebar from './Admin/Sidebar';
+// import Dashboard from './Admin/Dashbord';
+// import User from './Admin/User';
+// import Adduser from './Admin/Adduser';
+// import Roles from './Admin/Role';
+// import Category from './Admin/Category';
+// import AddSubCategory from './Admin/Subcategory';
+// import Product from './Admin/Product';
+// import Offers from './Admin/Offers';
+// import Charts from './Admin/Chart';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <HashRouter>
+//       <Routes>
+//         <Route path="/" element={<Sidebar />}>
+//           <Route path="login" element={<Login />} />
+//           <Route path="dashboard" element={<Dashboard />} />
+//           <Route path="charts" element={<Charts />} />
+//           <Route path="user" element={<User />} />
+//           <Route path="adduser" element={<Adduser />} />
+//           <Route path="role" element={<Roles />} />
+//           <Route path="category" element={<Category />} />
+//           <Route path="addSubCategory" element={<AddSubCategory />} />
+//           <Route path="offers" element={<Offers />} />
+//           <Route path="product" element={<Product />} />
+//         </Route>
+//       </Routes>
+//     </HashRouter>
+//   );
+// }
+
+// export default App;
+
+
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Login from './Admin/Login.jsx';
-import Sidebar from './Admin/Sidebar.jsx';
-import Dashboard from './Admin/Dashbord.jsx';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Login from './Admin/Login';
+import Sidebar from './Admin/Sidebar';
+import Dashboard from './Admin/Dashbord';
 import User from './Admin/User';
-import Adduser from './Admin/Adduser.jsx';
-import Roles from './Admin/Role.jsx';
+import Adduser from './Admin/Adduser';
+import Roles from './Admin/Role';
 import Category from './Admin/Category';
 import AddSubCategory from './Admin/Subcategory';
-import Product from './Admin/Product.jsx';
+import Product from './Admin/Product';
 import Offers from './Admin/Offers';
-import Charts from './Admin/Chart.jsx'
-import './App.css'
-
+import Charts from './Admin/Chart';
+import './App.css';
 
 function App() {
-
   return (
-  
-  <BrowserRouter>
-  
-   <Routes>
+    <HashRouter>
+     <Routes>
    <Route path='/'>
     
      <Route path='/' element={<Sidebar/>}></Route>
@@ -37,11 +116,8 @@ function App() {
     </Route>
    </Routes>
   
-   </BrowserRouter>
-  
-  )
+    </HashRouter>
+  );
 }
-export default App 
 
-
-
+export default App;
